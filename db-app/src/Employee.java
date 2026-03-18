03d0e3d46a07c3da53b93485b5046ec11f14ba23
al1zzadaa <alizadesadiq948@gmail.com>
2026-03-18 21:04:39 +0400

    add class Employee

---
+++ db-app/src/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    private String name;
+    private int age;
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+}
